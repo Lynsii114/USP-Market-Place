a9f9ad5b507fa3bdefcd6ae55a832cf199b6c8c0
Lyndray Duna <S11159184@student.usp.ac.fj>
2026-09-17 16:46:40 +1200

    two more categories added

--- frontend/src/data/categories.js
+++ frontend/src/data/categories.js
@@ -1,12 +1,16 @@
 import bookyImage from "../../Booky.jfif";
 import clothesImage from "../../Clothes.jfif";
 import electronicsImage from "../../Electronics.webp";
+import footwaresImage from "../../footwares.jpg";
+import homeAppliancesImage from "../../home appliances.jfif";
 import homeLivingImage from "../../Home and LIving.jpg";
 
 export const categories = [
   { id: 1, name: "Books", icon: "Book", image: bookyImage },
   { id: 2, name: "Electronics", icon: "Tech", image: electronicsImage },
   { id: 3, name: "Clothing", icon: "Wear", image: clothesImage },
-  { id: 4, name: "Furniture", icon: "Home", image: homeLivingImage },
-  { id: 5, name: "Other", icon: "More" },
+  { id: 4, name: "Footwares", icon: "Shoes", image: footwaresImage },
+  { id: 5, name: "Home Appliances", icon: "Appliance", image: homeAppliancesImage },
+  { id: 6, name: "Furniture", icon: "Home", image: homeLivingImage },
+  { id: 7, name: "Other", icon: "More" },
 ];
